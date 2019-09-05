9de656f303d3f8f365d7b7f4802992220b76c7df
EmilyZhangzzy <emilyzhangzzy@gmail.com>
2019-09-05 16:29:09 +0800

    add MainActivity linking to databaseapp/src/main/java/com/example/a54297/musicselect/activities/MainActivity.java

--- app/src/main/java/com/example/a54297/musicselect/activities/MainActivity.java
+++ app/src/main/java/com/example/a54297/musicselect/activities/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.a54297.musicselect.activities;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,18 +12,44 @@ import android.util.Log;
 import com.example.a54297.musicselect.Help.RealmHelp;
 import com.example.a54297.musicselect.R;
 import com.example.a54297.musicselect.adapters.MusicGridAdapter;
-import com.example.a54297.musicselect.adapters.MusicListAdapter;
+import com.example.a54297.musicselect.adapters.SongListAdapter;
 import com.example.a54297.musicselect.models.MusicSourceModel;
+import com.example.a54297.musicselect.models.SongModel;
 import com.example.a54297.musicselect.views.GridSpaceItemDecoration;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends BaseActivity {
 
     private RecyclerView mRvGrid, mRvList;
     private MusicGridAdapter mGridAdapter;
-    private MusicListAdapter mListAdaper;
+    private SongListAdapter mListAdaper;
     private RealmHelp mRealHelp;
     private MusicSourceModel mMusicSourceModel;
-
+    private List<SongModel> mDataSource;
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case 0:
+                    initView();
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +57,46 @@ public class MainActivity extends BaseActivity {
         setContentView(R.layout.activity_main);
 
         initData();
-        initView();
+
+        new Thread(){
+            @Override
+            public void run() {
+                String path= "http://47.98.168.203:8080/MusicSelectServer/hot";
+                final OkHttpClient client = new OkHttpClient();
+                Request request = new Request.Builder()
+                        .url(path)
+                        .build();
+                //开启一个异步请求
+                client.newCall(request).enqueue(new Callback() {
+                    //请求失败
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    //有响应
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        if (!response.isSuccessful()) {
+                            //请求失败
+                        }
+
+                        //获取到接口的数据
+                        String data = response.body().string();
+                        //把数据解析
+                        Gson mGson = new Gson();
+                        mDataSource = new ArrayList();
+                        mDataSource = mGson.fromJson(data, new TypeToken<List<SongModel>>() {
+                        }.getType());
+
+                        //通过handler运行initView
+                        Message msg = new Message();
+                        msg.what = 0;
+                        handler.sendMessage(msg);
+                    }
+                });
+            }
+        }.start();
     }
 
     private void initData(){
@@ -57,7 +124,7 @@ public class MainActivity extends BaseActivity {
         mRvList.setLayoutManager(new LinearLayoutManager(this));
         mRvList.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL));
         mRvGrid.setNestedScrollingEnabled(false);
-        mListAdaper = new MusicListAdapter(this,mRvList,mMusicSourceModel.getHot());
+        mListAdaper = new SongListAdapter(this,mRvList,mDataSource);
         mRvList.setAdapter(mListAdaper);
     }
 
@@ -66,5 +133,4 @@ public class MainActivity extends BaseActivity {
         super.onDestroy();
         mRealHelp.close();
     }
-
 }
